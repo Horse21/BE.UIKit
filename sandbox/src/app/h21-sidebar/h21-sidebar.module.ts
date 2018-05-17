aecc5b935b9ec8e1843e29c94b01f196ca240c66
kamil_gadjiev <kamil.gadjiev@gmail.com>
2018-05-17 10:24:19 +0300

    fix: build

--- sandbox/src/app/h21-sidebar/h21-sidebar.module.ts
+++ sandbox/src/app/h21-sidebar/h21-sidebar.module.ts
@@ -2,14 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {H21SidebarComponent} from "./h21-sidebar.component";
 import {MatButtonModule, MatIconModule} from "@angular/material";
-import {H21DocsNavigationModule} from "../docs-navigation/h21-docs-navigation.module";
 
 @NgModule({
 	imports: [
 		CommonModule,
 		MatButtonModule,
-		MatIconModule,
-		H21DocsNavigationModule
+		MatIconModule
 	],
 	declarations: [H21SidebarComponent],
 	exports: [H21SidebarComponent]
