28b3acb9de2c7197a906d5e49ddec4a430768519
Gadzhiev Magomed <gadzhiev.m.m@gmail.com>
2018-05-23 17:50:53 +0300

    fix

--- sandbox/projects/h21-be-ui-kit/src/public_api.ts
+++ sandbox/projects/h21-be-ui-kit/src/public_api.ts
@@ -5,3 +5,4 @@ export * from './lib/h21-breadcrumbs/h21-breadcrumbs.module';
 export * from './lib/h21-header/h21-header.module';
 export * from './lib/h21-sidebar/h21-sidebar.module';
 export * from './lib/h21-top-toolbar/h21-top-toolbar.module';
+export * from './lib/h21-search-panel/h21-search-panel.module';
