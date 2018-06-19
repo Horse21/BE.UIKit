0b50c2ca24d407abf2aceaba9a221e129b136ec2
Gadzhiev Magomed <gadzhiev.m.m@gmail.com>
2018-06-19 18:47:54 +0300

    Делал компонент выбора даты h21-two-month-calendar, в процессе.

--- sandbox/projects/h21-be-ui-kit/src/lib/h21-two-month-calendar/h21-two-month-calendar.component.ts
+++ sandbox/projects/h21-be-ui-kit/src/lib/h21-two-month-calendar/h21-two-month-calendar.component.ts
@@ -1,19 +1,10 @@
-import { Component, ViewChild, Input, Output, EventEmitter, Inject, ElementRef } from "@angular/core";
-//import { MatCalendar } from "@angular/material";
-//import { DateAdapter, MAT_DATE_FORMATS, MatDateFormats } from '@angular/material/core';
-import {animate, state, style, transition, trigger} from "@angular/animations";
+import { Component, ViewChild, Input, Output, EventEmitter, Inject, ElementRef, Renderer2 } from "@angular/core";
+import { MatCalendar } from "@angular/material";
+import { DateAdapter, MAT_DATE_FORMATS, MatDateFormats } from '@angular/material/core';
 
 @Component ({
 	selector: 'h21-two-month-calendar',
-	templateUrl: './h21-two-month-calendar.component.html',
-	animations: [
-		trigger('sliderMove', [
-			state('staic', style({transform: 'translateX(0)'})),
-			state('movePrev', style({transform: 'translateX(0)'})),
-			state('moveNext',style({transform: 'translateX(0)'})),
-			transition('* => *', animate('120ms')),
-		])
-	]
+	templateUrl: './h21-two-month-calendar.component.html'
 })
 
 export class H21TwoMonthCalendarComponent {
@@ -23,14 +14,27 @@ export class H21TwoMonthCalendarComponent {
 	@Output() selectedDate: EventEmitter<Date> = new EventEmitter();
 
 	@ViewChild('twcSliderItemsBox') elementView: ElementRef;
+	@ViewChild('MatCalendar') calendar: MatCalendar<Date>;
 
-	constructor () {
-
+	constructor (
+		private renderer: Renderer2,
+		@Inject(MAT_DATE_FORMATS) private _dateFormats: MatDateFormats,
+		private dateAdapter: DateAdapter<Date>
+	) {
+		this.todayDate = this.dateAdapter.today();
+		console.log(this.todayDate);
 	}
 
-	sliderItemsCount = 12;
+	todayDate: Date;
+	// startDates: Date[] = [];
+	// finishDates: Date[] = [];
+
+	test = new Date(2018, 6, 25);
+
+	sliderItemsCount = 7;
 	sliderItemsBoxWidth = 0;
 	sliderItemWidth = 0;
+	sliderCurrentIndex = 0;
 	sliderCurrentTranslation = 0;
 
 	ngAfterViewInit() {
@@ -39,12 +43,30 @@ export class H21TwoMonthCalendarComponent {
 	}
 
 	prevSlide() {
-		this.sliderCurrentTranslation -= this.sliderItemWidth;
-
+		this.sliderCurrentIndex--;
+		this.moveSlide();
 	}
 
 	nextSlide() {
-		this.sliderCurrentTranslation += this.sliderItemWidth;
+		this.sliderCurrentIndex++;
+		this.moveSlide();
+	}
 
+	moveSlide() {
+		this.sliderCurrentTranslation = this.sliderCurrentIndex * this.sliderItemWidth;
+		this.renderer.setStyle(this.elementView.nativeElement, 'transform', 'translateX(' + String( -this.sliderCurrentTranslation ) + 'px)');
+	}
+
+	getMinDate(i) {
+		let monthBegin = new Date(2018, i-1, 1);
+		return monthBegin; // > this.todayDate ? this.todayDate : monthBegin;
+	}
+
+	getCalendarList() {
+		var arr = new Array<number>();
+		let date = this.fromDate;
+		while (date < this.toDate) {
+			//arr.push(this.dateAdapter.getMonth());
+		}
 	}
 }
