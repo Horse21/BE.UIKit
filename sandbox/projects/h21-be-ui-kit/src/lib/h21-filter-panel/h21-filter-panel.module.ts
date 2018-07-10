cf0ed7f2b2722809919408fbffe02840874c6bd2
kamil_gadjiev <kamil.gadjiev@gmail.com>
2018-07-10 16:13:41 +0300

    be.uikits module import

--- sandbox/projects/h21-be-ui-kit/src/lib/h21-filter-panel/h21-filter-panel.module.ts
+++ sandbox/projects/h21-be-ui-kit/src/lib/h21-filter-panel/h21-filter-panel.module.ts
@@ -1,15 +1,16 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {H21FilterPanelComponent} from './h21-filter-panel.component';
-import {MatCheckboxModule, MatExpansionModule} from "@angular/material";
-import {NouisliderModule} from 'ng2-nouislider';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { H21FilterPanelComponent } from './h21-filter-panel.component';
+import { MatButtonModule, MatCheckboxModule, MatExpansionModule } from '@angular/material';
+import { NouisliderModule } from 'ng2-nouislider';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		MatExpansionModule,
 		MatCheckboxModule,
-		NouisliderModule
+		NouisliderModule,
+		MatButtonModule
 	],
 	declarations: [H21FilterPanelComponent],
 	exports: [H21FilterPanelComponent]
