7902d2476eaf4b84965fefdc4463b2eed1b11bd9
Gadzhiev Magomed <gadzhiev.m.m@gmail.com>
2018-07-11 19:37:01 +0300

    Переделал компонент отображения примера docs-example-viewer, убрал используемый warkdown компонент, сделал подцветку кода на базе PrismJs

--- sandbox/src/app/docs-example-viewer/docs-example-viewer.component.ts
+++ sandbox/src/app/docs-example-viewer/docs-example-viewer.component.ts
@@ -1,51 +1,113 @@
 import {Component, Input} from "@angular/core";
-import {Observable, Observer} from 'rxjs';
-import { NgxMdService } from 'ngx-md';
-import { map } from 'rxjs/operators';
-export interface ExampleTab {
-	label: string;
-	content: string;
-}
+import {Observable} from 'rxjs';
+import {HttpClient} from "@angular/common/http";
+import {catchError, map} from 'rxjs/operators';
+import {throwError} from "rxjs/index";
 
-import 'prismjs/prism';
+import * as Prism from 'prismjs';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
 
-@Component ({
+@Component({
 	selector: 'docs-example-viewer',
 	templateUrl: './docs-example-viewer.component.html',
 })
 
 export class DocsExampleViewerComponent {
-	asyncTabs: Observable<ExampleTab[]>;
 
+	/** Заголовок */
 	@Input() title: string;
-	@Input() htmlCodePath: string;
-	@Input() tsCodePath: string;
-	@Input() cssCodePath: string;
+	/** Путь к файлу c примером HTML кода */
+	@Input() pathToHtml: string;
+	/** Путь к файлу c примером Type Script кода */
+	@Input() pathToTs: string;
+	/** Путь к файлу c примером CSS кода */
 
+	@Input() pathToCss: string;
+
+	/**
+	 * Индикатор видимости
+	 * @type {boolean}
+	 */
 	viewExampleCode: boolean = false;
 
-	constructor (private _markdown: NgxMdService) {
-		//console.log(this._markdown.getContent(this.htmlCodePath).);
+	codeHtml: string = "";
+	codeTs: string = "";
+	codeCss: string = "";
 
-		this.asyncTabs = Observable.create((observer: Observer<ExampleTab[]>) => {
-			setTimeout(() => {
-				observer.next([
-					{label: 'HTML', content: `<ngx-md [path]="htmlCodePath"> </ngx-md>`},
-					{label: 'TS', content: '<ngx-md [path]="tsCodePath"> </ngx-md>'},
-					{label: 'CSS', content: '<ngx-md [path]="cssCodePath"> </ngx-md>'},
-				]);
-			}, 1000);
-		});
+	private _code;
+
+	constructor(private _http: HttpClient) {
 	}
 
-	ngOnInit() {
-
+	ngAfterViewInit() {
+		if (this.pathToHtml) {
+			this.getContent(this.pathToHtml, 'HTML');
+		}
+		if (this.pathToTs) {
+			this.getContent(this.pathToTs, 'TS');
+		}
+		if (this.pathToCss) {
+			this.getContent(this.pathToCss, 'CSS');
+		}
 	}
 
+	getRemoteContent(path: string): Observable<any> {
+		return this._http.get(path, {responseType: 'text'}).pipe(map(res => res)).pipe(catchError(this.handleError));
+	}
+
+	getContent(path: string, lang: string) {
+		this.getRemoteContent(path).subscribe(data => {
+			switch (lang) {
+				case 'TS':
+					this.codeTs = DocsExampleViewerComponent.highlightCode(data, 'TS');
+					break;
+				case 'CSS':
+					this.codeCss = DocsExampleViewerComponent.highlightCode(data, 'CSS');
+					break;
+				default :
+					this.codeHtml = DocsExampleViewerComponent.highlightCode(data, 'Html');
+			}
+		})
+	}
+
+	private handleError(error: any): any {
+		let errMsg: string;
+		if (error instanceof fetch) {
+			const body = error.json() || '';
+			const err = body.error || JSON.stringify(body);
+			errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+		} else {
+			errMsg = error.message ? error.message : error.toString();
+		}
+		return throwError(errMsg);
+	}
+
+	/**
+	 *
+	 */
 	viewExampleCodeToggle() {
 		this.viewExampleCode = !this.viewExampleCode;
+	}
+
+	/**
+	 *
+	 * @param {string} Програмный код
+	 * @param {string} Язык кода
+	 * @returns {string} Строка с HTML разметкой
+	 */
+	private static highlightCode(code: string, lang: string = 'HTML'): string {
+		if (code) {
+			switch (lang) {
+				case 'TS':
+					return Prism.highlight(code, Prism.languages.typescript);
+				case 'CSS':
+					return Prism.highlight(code, Prism.languages.css);
+				default :
+					return Prism.highlight(code, Prism.languages.html);
+			}
+		}
+		return '';
 	}
 }
--- sandbox/src/app/docs/two-month-calendar/two-month-calendar-docs.component.ts
+++ sandbox/src/app/docs/two-month-calendar/two-month-calendar-docs.component.ts
@@ -8,4 +8,19 @@ import {Component} from "@angular/core";
 export class TwoMonthCalendarDocsComponent {
 	/** Section title */
 	title = 'Two month calendar component';
+
+	starDate: Date;
+	finishDate: Date;
+	starText: string;
+	finishText: string;
+	selectedDate: Date;
+
+	constructor() {
+		this.starDate = new Date();
+		this.finishDate = new Date(this.starDate.getFullYear(), this.starDate.getMonth() + 4, this.starDate.getDay());
+		this.starText = "Start";
+		this.finishText = "Finish";
+		this.selectedDate = new Date(this.starDate.getFullYear(), this.starDate.getMonth(), this.starDate.getDay())
+	}
 }
+
