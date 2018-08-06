84e12cbf1a4692a2f92a9850b5da9b55a5e29ef3
Manxome <manxome@Manxomes-iMac.local>
2018-08-06 18:28:23 +0300

    Таблица путешественника

--- sandbox/src/app/docs/h21-table-travelers/h21-table-travelers.component.ts
+++ sandbox/src/app/docs/h21-table-travelers/h21-table-travelers.component.ts
@@ -11,5 +11,25 @@ export class H21TableTravelersComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  displayedColumns: string[] = ['avatar', 'name', 'agency', 'branch', 'agent', 'status', 'action'];
+  dataSource = ELEMENT_DATA;
 }
+export interface TableTravelers {
+  avatar: string;
+  name: string;
+  agency: string;
+  branch: string;
+  agent: string;
+  status: string;
+}
+
+const ELEMENT_DATA: TableTravelers[] = [
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'},
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'},
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'},
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'},
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'},
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'},
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'},
+  {avatar: 'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg', name: 'Jeff Bezos', agency: 'Raptim', agent:'Jane Doe', branch: 'Raptim office Denmark', status: 'Active'}
+];
