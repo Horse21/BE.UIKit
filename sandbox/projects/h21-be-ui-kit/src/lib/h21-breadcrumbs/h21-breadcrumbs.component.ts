cdeb098053ce7950eee4ffc14dfa7119185f8d30
Gadzhiev Magomed <gadzhiev.m.m@gmail.com>
2018-08-08 16:36:20 +0300

    Правил компонент h21-breadcrumbs Добавил возмоность исользования routerLink в ссылках

--- sandbox/projects/h21-be-ui-kit/src/lib/h21-breadcrumbs/h21-breadcrumbs.component.ts
+++ sandbox/projects/h21-be-ui-kit/src/lib/h21-breadcrumbs/h21-breadcrumbs.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from "@angular/core"
-import {IBreadcrumb} from "./../../dto/i-breadcrumb";
+import { Component, Input } from "@angular/core"
+import { IBreadcrumb } from "./../../dto/i-breadcrumb";
 
 @Component({
 	selector: 'h21-breadcrumbs',
@@ -8,8 +8,14 @@ import {IBreadcrumb} from "./../../dto/i-breadcrumb";
 
 export class H21BreadcrumbsComponent {
 
+	/** Specifies whether the last element should be a link */
 	@Input() lastIsLink: boolean = false;
+
+	/** An array of breadcrumbs in the form of objects { url: '', label: ''} */
 	@Input() breadcrumbsData: Array<IBreadcrumb>;
+
+	/** The way to go by link. The possible values are 'href' and 'routerLink' */
+	@Input() linkMode: 'href' | 'routerLink' = 'href';
 
 	constructor () {
 
--- sandbox/projects/h21-be-ui-kit/src/lib/h21-breadcrumbs/h21-breadcrumbs.module.ts
+++ sandbox/projects/h21-be-ui-kit/src/lib/h21-breadcrumbs/h21-breadcrumbs.module.ts
@@ -2,14 +2,18 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatIconModule} from "@angular/material";
 import {H21BreadcrumbsComponent} from "./h21-breadcrumbs.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
 	imports: [
 		CommonModule,
 		MatIconModule,
+		RouterModule
 	],
 	declarations: [H21BreadcrumbsComponent],
 	exports: [H21BreadcrumbsComponent]
 })
+
 export class H21BreadcrumbsModule {
+
 }
